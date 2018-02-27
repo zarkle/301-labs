67ffb42b3a8e87f410bbfe17c714e81f00d76ad0
Beverly Pham <beverly.pham@gmail.com>
2018-02-26 20:51:36 -0800

    updated solution code

--- 10-functional-programming/solution/server.js
+++ 10-functional-programming/solution/server.js
@@ -7,7 +7,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 3000;
 const app = express();
-const conString = 'postgres://localhost:5432/articles';
+const conString = 'postgres://localhost:5432/kilovolt';
 const client = new pg.Client(conString);
 client.connect();
 client.on('error', err => {
